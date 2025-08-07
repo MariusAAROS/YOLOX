f69fba16f9d1625dd83f2a5f4f32b724a328329f
Marius ORTEGA <m.ortega@groupeonepoint.com>
2025-08-07 12:31:23 +0200

    format script for Camel

--- preprocessing/format_camel.py
+++ preprocessing/format_camel.py
@@ -3,13 +3,26 @@ import shutil
 import json
 import random
 
-train_test_ratio = 0.8
-human_index = [1, 2, 3, 4, 5, 6, 7, 8, 13, 20,
-                21,24, 25, 26, 27, 28, 29, 30]
-train_index = random.sample(human_index, int(len(human_index) * train_test_ratio))
-val_index = list(set(human_index) - set(train_index))
+NEW_SPLIT = False
+split_path = "datasets/Camel/split.txt"
+
+if NEW_SPLIT or not os.path.exists(split_path):
+    train_test_ratio = 0.8
+    human_index = [1, 2, 3, 4, 5, 6, 7, 8, 13, 20,
+                    21, 25, 26, 27, 28, 30]
+    train_index = random.sample(human_index, int(len(human_index) * train_test_ratio))
+    val_index = list(set(human_index) - set(train_index))
+    with open("datasets/Camel/split.txt", "w") as f:
+        f.write("Train: " + ", ".join(map(str, train_index)) + "\n")
+        f.write("Val: " + ", ".join(map(str, val_index)) + "\n")
+elif not NEW_SPLIT and os.path.exists(split_path):
+    with open(split_path, "r") as f:
+        lines = f.readlines()
+        train_index = list(map(int, lines[0].strip().split(": ")[1].split(", ")))
+        val_index = list(map(int, lines[1].strip().split(": ")[1].split(", ")))
+
 exclude = ["train2017", "val2017", "annotations"]
-img_typ = "IR" # IR or Vis
+img_typ = "Vis" # IR or Vis
 
 dataset_name = "Camel"
 data_path = f"datasets/{dataset_name}"
@@ -24,7 +37,12 @@ annotations = {
     "info": {
         "description": f"{dataset_name} {img_typ} dataset"
     },
-    "licenses": [],
+    "licenses": [
+        {
+            "id": 1,
+            "name": "Unknown",
+            "url": "Unknown"}
+    ],
     "images": [],
     "annotations": [],
     "categories": [
@@ -39,8 +57,8 @@ annotations = {
 
 dirs_seqs = os.listdir(data_path)
 
-id_counter = 1
 for subset_name, subset_index in zip(["train2017", "val2017"], [train_index, val_index]):
+    id_counter = 1
     for cdir in dirs_seqs:
         if cdir not in exclude:
             try:
@@ -60,7 +78,9 @@ for subset_name, subset_index in zip(["train2017", "val2017"], [train_index, val
                     if f'{"Visual" if img_typ == "Vis" else "IR"} Resolution' in line:
                         width, height = line.split(" - ")[1].strip().split("x")
                         width, height = int(width), int(height)
-
+                        break
+                with open(os.path.join(data_path, cdir, f"Seq{index}-{img_typ}.txt"), "r") as f:
+                    annots = f.readlines()
                 for cimg in cimgs_files:
                     #copy image to target directory
                     new_name = f"{index}_{cimg}"
@@ -75,16 +95,18 @@ for subset_name, subset_index in zip(["train2017", "val2017"], [train_index, val
                         "width": width,
                         "height": height,
                     })
-
-                    #TO DO - add the annotations 
-                    annotations["annotations"].append({
-                        'image_id': id_counter,
-                        'bbox': [214.14976, 41.29011799999999, 348.25984, 243.78],
-                        'category_id': 1,
-                        'id': id_counter
-                    })
-
                     id_counter += 1
+
+                for line in annots:
+                    if line.strip():
+                        parts = line.strip().split("\t")
+                        frame_id, object_id, object_class = map(int, parts[:3])
+                        x1, y1, x2, y2 = map(float, parts[3:])
+                        annotations["annotations"].append({
+                            'image_id': frame_id,
+                            'bbox': [x1, y1, x2, y2],
+                            'category_id': object_class,
+                            'id': object_id
+                        })
                 with open(os.path.join(target_path, "annotations", f"instances_{subset_name}.json"), "w") as f:
                     json.dump(annotations, f, indent=4)
-                break
