7298e08de6f4f195f7eaa8a704826c6ed9f95796
Marius ORTEGA <m.ortega@groupeonepoint.com>
2025-08-08 15:29:10 +0200

    offset correction

--- preprocessing/format_camel.py
+++ preprocessing/format_camel.py
@@ -84,7 +84,7 @@ for subset_name, subset_index in zip(["train2017", "val2017"], [train_index, val
                         break
                 with open(os.path.join(data_path, cdir, f"Seq{index}-{img_typ}.txt"), "r") as f:
                     annots = f.readlines()
-                ids_offset = id_counter
+                ids_offset = id_counter - 1
                 for cimg in cimgs_files:
                     #copy image to target directory
                     new_name = f"{index}_{cimg}"
